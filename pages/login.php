21498b47a8b92997a29ed2d9674fa790aa33286d
thomas <thomas.degreve@condorcet.Be>
2024-05-19 13:09:19 +0200

    avancement

--- pages/login.php
+++ pages/login.php
@@ -21,7 +21,7 @@ if (isset($_POST['submit_login'])) {
 ?>
 
 
-    <title>Page de Connexion</title>
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
 <!-- Formulaire de connexion -->
